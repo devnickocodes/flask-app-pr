f07397816a00ef3dbd24623a6147e50f452b4fff
Nikolay Hristev <devnickocodes@gmail.com>
2024-02-17 15:24:18 +0200

    add the templates folder with the index and about html files

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # creates an instance of this Flask class
 
 @app.route("/") # we use the route decorator to tell Flask what URL should trigger the function that follows # "/" is the root directory when we try to browse it Flask triggers the index function
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(
