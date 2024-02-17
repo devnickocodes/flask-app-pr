6b15fc3f8fd3e01200cf3cf5c4f36ac0c1915dc8
Nikolay Hristev <devnickocodes@gmail.com>
2024-02-17 16:05:14 +0200

    using jinja syntax adjust the templates so that they inherit from base.html

--- run.py
+++ run.py
@@ -1,15 +1,28 @@
 import os
 from flask import Flask, render_template
 
+
 app = Flask(__name__) # creates an instance of this Flask class
+
 
 @app.route("/") # we use the route decorator to tell Flask what URL should trigger the function that follows # "/" is the root directory when we try to browse it Flask triggers the index function
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True # when submitting change it to FALSE
-    )
+    ) 
