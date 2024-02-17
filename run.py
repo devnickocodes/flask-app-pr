46b350ad610a87132bd36c8722560c86ccc802ac
Nikolay Hristev <devnickocodes@gmail.com>
2024-02-17 14:49:31 +0200

    import the os module for environment variablds to deploy locally the app, once deployed it returns 'Hello, World!'

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # creates an instance of this Flask class
+
+@app.route("/") # we use the route decorator to tell Flask what URL should trigger the function that follows # "/" is the root directory when we try to browse it Flask triggers the index function
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True # when submitting change it to FALSE
+    )
