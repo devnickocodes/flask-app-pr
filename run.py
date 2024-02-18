7812db534f9dc5d41c37e3a2bdd07ad32c3a8f90
Nikolay Hristev <devnickocodes@gmail.com>
2024-02-18 14:56:49 +0200

    update flask functionality

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json 
 from flask import Flask, render_template
 
 
@@ -12,17 +13,31 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company = data) #company is a new variable that will be sent through to the HTML template which is equal to the list of data it's loading
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return "<h1>" + member["name"]+ "</h1>"
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
